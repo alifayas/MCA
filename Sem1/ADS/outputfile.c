036113051ed660602ee84aee3202a9cd85d4f350
ALI AKBAR FAYAS <145116508+alifayas@users.noreply.github.com>
2023-12-08 12:01:44 +0530

    Add files via upload

---
+++ Sem1/ADS/outputfile.c
@@ -0,0 +1,750 @@
+DS 1
+enter the size of array a:
+4
+enter the array elements:
+9 8 7 6
+enter the size of array b:
+3
+enter the array elements:
+5 4 2
+
+array a:
+6789
+array b:
+245
+array c:
+2	4	5	6	7	8	9	
+
+
+
+DS 2
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 1
+Enter the element to insert: 5
+
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 1
+Enter the element to insert: 6
+
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 1
+Enter the element to insert: 7
+
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 2
+Deleted element: 5
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 3
+Front = 1
+Rear = 2
+6 7 
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 4
+Enter the element to search : 7
+Element found at position 2
+
+1: Insertion
+2: Deletion
+3: Display
+4: Search
+5: Exit
+Enter your choice: 5
+
+
+
+DS 3
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 1
+
+Enter the value : 1
+
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 1
+
+Enter the value : 2
+
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 1
+
+Enter the value : 3
+
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 1
+
+Enter the value : 4
+
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 2
+Element popped - 4
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 3
+
+Enter element : 2
+
+Item found at position 2
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 4
+the list is
+3-->2-->1-->
+
+1:Push
+2:Pop
+3:Linear search
+4:Display
+5:Exit
+Enter your choice : 5
+
+
+
+DS 4
+1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+1
+
+list is empty
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+2
+
+enter number to be inserted:
+2
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+3
+
+enter number to be inserted:
+4
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+4
+enter position and data:
+3
+7
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+2
+
+enter number to be inserted:
+8
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+2
+
+enter number to be inserted:
+9
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+5
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+6
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+7
+
+enter position:
+3
+
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+1
+
+the list is
+8-->2-->
+	1.to see list
+	2.for insertion at front
+	3.for insertion at end
+	4.for insertion at any position
+	5.for deletion at front
+	6.for deletion at end
+	7.for deletion at any position
+	8.to exit
+	enter choice
+8
+
+
+
+DS 5
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 1
+
+Enter value : 1
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 1
+
+Enter value : 2
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 2
+
+Enter value : 4
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 2
+
+Enter value : 3
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 3
+
+Enter value : 6
+
+Enter the position : 2
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 3
+
+Enter value : 7
+
+Enter the position : 3
+
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 4
+
+2 deleted
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 5
+
+3 deleted
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 6
+
+Enter position : 2
+
+4 deleted
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 7
+
+Enter element to search : 7
+
+Item found at position 3
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 8
+1-> 6-> 7-> null
+
+1:Insert at beginning
+2:Insert at last
+3:Insert at position
+4:Delete from beginning
+5:Delete from last
+6:Delete from position
+7:Search
+8:Display
+9:Exit
+Enter your choice : 9
+
+
+
+DS 6
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 1
+
+Enter a number : 5
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 2
+
+Enter a number : 4
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 2
+
+Enter a number : 6
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 3
+
+Enter a number : 4
+
+FOUND!
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 4
+
+..................................
+5 ->4 ->6 ->
+..................................
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 5
+
+Enter a number to be deleted : 4
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 4
+
+..................................
+5 ->6 ->
+..................................
+
+1)Create Root Node 
+2)Insert Node
+3)Search
+4)preorderTraversal 
+5)Delete 
+6)Quiet 
+Choose Option :: 6
+
+
+
+DS 7
+Enter size of universal set : 7
+Enter elements : 1 2 3 4 5 6 7
+
+Enter size of set 1 : 1
+
+Enter elements : 2
+
+Enter size of set 2 : 3
+
+Enter elements : 2 3 4
+
+Universal set : {1,2,3,4,5,6,7,}
+Set 1 bit string : {0,1,0,0,0,0,0,}
+Set 2 bit string : {0,1,1,1,0,0,0,}
+Set 1 : {2,}
+Set 2 : {2,3,4,}
+Union : {2,3,4,}
+Intersection : {2,}
+Difference : {}
+
+
+
+DS 8
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 1
+Enter element : 1
+
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 1
+Enter element : 2
+
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 2
+The sets are
+1 2 
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 4
+Enter the value to find : 1
+
+Value 1 is in set 1
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 3
+enter the two sets to union
+Enter first set : 1
+Enter second set : 2
+
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 4
+Enter the value to find : 2
+
+Value 2 is in set 1
+
+1:Make Set
+2:Display set Representatives
+3:Union
+4:Find Set
+5:Exit
+Enter your choice : 5
+
+
+
+DS 9
+Enter the size of the array: 5
+Enter the elements in the array:
+22
+44
+55
+12
+4
+The array is : 22	44	55	12	4	
+ Largest Number is : 55
+ Smallest Number is : 4
+
+
+
+DS 10
+Enter the size of STACK[MAX=100]:10
+
+	 STACK OPERATIONS USING ARRAY
+	 1.PUSH
+	 2.POP
+	 3.DISPLAY
+	 4.EXIT
+ Enter the Choice:1
+ Enter a value to be pushed:2
+
+ Enter the Choice:1
+ Enter a value to be pushed:4
+
+ Enter the Choice:2
+
+	 The popped elements is 4
+ Enter the Choice:3
+
+ The elements in STACK 
+
+2
+ Press Next Choice
+ Enter the Choice:4
+
+	 EXIT POINT 
+
+
+
+DS 11
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 1
+Enter the element to enqueue: 2
+2 enqueued to the queue.
+
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 1
+Enter the element to enqueue: 3
+3 enqueued to the queue.
+
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 1
+Enter the element to enqueue: 5
+5 enqueued to the queue.
+
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 2
+2 dequeued from the queue.
+
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 3
+Elements in the queue: 3 5 
+
+Queue Menu:
+1. Enqueue
+2. Dequeue
+3. Display
+4. Quit
+Enter your choice: 4
+Exiting the program.
+
